421b4149bc4fe39afa80e0caf35009ea3318e600
DESKTOP-CLC1HGE\xamohsen <ahmedmohsen106@gmail.com>
2017-06-05 03:46:37 +0200

    init commit

---
+++ src/Hoba/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Hoba.Controllers
+{
+    public class ImageController : Controller
+    {
+        public ImageController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        private IHostingEnvironment _environment;
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ICollection<IFormFile> files)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
